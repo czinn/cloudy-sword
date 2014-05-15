b2449b4aa82f07ef8f5ece2f438b4eed9ac03973
Charles <charles.zinn@gmail.com>
2014-05-15 10:49:23 -0400

    Replaced all tabs with four spaces for consistency.

--- static/gamestate.js
+++ static/gamestate.js
@@ -7,35 +7,35 @@
   
 // Require map.js and tile.js if this is the server; if it is the client, the client will have already loaded it
 if(typeof exports === "object") {
-	var Map = require("./map.js");
-	var Tile = require("./tile.js");
+    var Map = require("./map.js");
+    var Tile = require("./tile.js");
 }
   
 /** Creates a new game state object for the given number of players */
 var GameState = function(players) {
-	// Create a new map and make it a hexagon
-	this.map = new Map(15, 15);
-	this.map.makeHexShape();
-	
-	this.numPlayers = players;
-	
-	this.clearTurn();
+    // Create a new map and make it a hexagon
+    this.map = new Map(15, 15);
+    this.map.makeHexShape();
+    
+    this.numPlayers = players;
+    
+    this.clearTurn();
 }
 
 /** Dumps the game state to a simpler JavaScript object
   * This is used to send the state over the network.
   */
 GameState.prototype.dump = function() {
-	obj = {numPlayers: this.numPlayers, map: this.map};
+    obj = {numPlayers: this.numPlayers, map: this.map};
 
-	return obj;
+    return obj;
 };
 
 /** Loads the game state from a JavaScript object */
 GameState.prototype.load = function(obj) {
-	this.numPlayers = obj.players;
-	this.map = new Map(1, 1);
-	this.map.load(obj.map);
+    this.numPlayers = obj.players;
+    this.map = new Map(1, 1);
+    this.map.load(obj.map);
 };
 
 /** Clears the local turn.
@@ -44,38 +44,38 @@ GameState.prototype.load = function(obj) {
   * All relevant changes to the game state should be reflected in the current turn
   */
 GameState.prototype.clearTurn = function() {
-	// For now, the flip array is a list of tiles to be flipped (since that's the only action right now)
-	this.localTurn = {flip: []};
+    // For now, the flip array is a list of tiles to be flipped (since that's the only action right now)
+    this.localTurn = {flip: []};
 };
 
 /** Applies the given turn object to the state
   * Used to change from one state to a new, similar state, so that the server doesn't have to resend all data
   */
 GameState.prototype.doTurn = function(obj) {
-	// Currently, for testing purposes, obj only contains obj.flip, which is a list of tiles to flip	
-	for(var i = 0; i < obj.flip.length; i++) {
-		var tile = obj.flip[i];
-		
-		if(this.map.terrain[tile.y][tile.x] == Tile.NORMAL)
-			this.map.terrain[tile.y][tile.x] = Tile.WALL;
-		else if(this.map.terrain[tile.y][tile.x] == Tile.WALL)
-			this.map.terrain[tile.y][tile.x] = Tile.NORMAL;
-	}
+    // Currently, for testing purposes, obj only contains obj.flip, which is a list of tiles to flip    
+    for(var i = 0; i < obj.flip.length; i++) {
+        var tile = obj.flip[i];
+        
+        if(this.map.terrain[tile.y][tile.x] == Tile.NORMAL)
+            this.map.terrain[tile.y][tile.x] = Tile.WALL;
+        else if(this.map.terrain[tile.y][tile.x] == Tile.WALL)
+            this.map.terrain[tile.y][tile.x] = Tile.NORMAL;
+    }
 };
 
 /** Does an action; updates the game state and applies the change to localTurn */
 GameState.prototype.doAction = function(action) {
-	// For now, action is basically just a tile to be flipped
-	var tile = action.tile;
-	if(this.map.terrain[tile.y][tile.x] == Tile.NORMAL)
-		this.map.terrain[tile.y][tile.x] = Tile.WALL;
-	else if(this.map.terrain[tile.y][tile.x] == Tile.WALL)
-		this.map.terrain[tile.y][tile.x] = Tile.NORMAL;
-		
-	// Add the tile to localTurn
-	this.localTurn.flip.push(tile);
+    // For now, action is basically just a tile to be flipped
+    var tile = action.tile;
+    if(this.map.terrain[tile.y][tile.x] == Tile.NORMAL)
+        this.map.terrain[tile.y][tile.x] = Tile.WALL;
+    else if(this.map.terrain[tile.y][tile.x] == Tile.WALL)
+        this.map.terrain[tile.y][tile.x] = Tile.NORMAL;
+        
+    // Add the tile to localTurn
+    this.localTurn.flip.push(tile);
 };
 
 if(typeof exports === "object") {
-	module.exports = GameState;
+    module.exports = GameState;
 }
--- static/map.js
+++ static/map.js
@@ -5,7 +5,7 @@
   
 // Require tile.js if this is the server; if it is the client, the client will have already loaded it
 if(typeof exports === "object") {
-	var Tile = require("./tile.js");
+    var Tile = require("./tile.js");
 }
 
 // Declare constants
@@ -15,14 +15,14 @@ var HEX_HEIGHT = 200; //pixels
   * Creates an empty map of the specified dimensions.
   */
 var Map = function(rows, cols) {
-	//Create an normal terrain instead of empty for testing
-	this.terrain = [];
-	for(var row = 0; row < rows; row++) {
-		this.terrain[row] = [];
-		for(var col = 0; col < cols; col++) {
-			this.terrain[row][col] = Math.random() > 0.2 ? Tile.NORMAL : Tile.WALL
-		}
-	}
+    //Create an normal terrain instead of empty for testing
+    this.terrain = [];
+    for(var row = 0; row < rows; row++) {
+        this.terrain[row] = [];
+        for(var col = 0; col < cols; col++) {
+            this.terrain[row][col] = Math.random() > 0.2 ? Tile.NORMAL : Tile.WALL
+        }
+    }
 }
 
 /** Calculates the distance between the given hexes
@@ -30,89 +30,89 @@ var Map = function(rows, cols) {
   * Does not take into account non-walkable hexes
   */
 Map.prototype.distance = function(x1, y1, x2, y2) {
-	return Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1), Math.abs(y2 + x2 - y1 - x1));
+    return Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1), Math.abs(y2 + x2 - y1 - x1));
 };
 
 /** Chops corners off to make the map shaped like a large hexagon
   * Only works properly if rows and cols are odd and equal to each other
   */
 Map.prototype.makeHexShape = function() {
-	// Size of the corners to chop off
-	var cornerSize = (this.rows() - 1) / 2;
-	
-	for(var r = 0; r < this.rows(); r++) {
-		for(var c = 0; c < this.cols(); c++) {
-			if(this.distance(0, 0, r, c) < cornerSize || this.distance(this.rows() - 1, this.cols() - 1, r, c) < cornerSize) {
-				this.terrain[r][c] = Tile.EMPTY;
-			}
-		}
-	}
+    // Size of the corners to chop off
+    var cornerSize = (this.rows() - 1) / 2;
+    
+    for(var r = 0; r < this.rows(); r++) {
+        for(var c = 0; c < this.cols(); c++) {
+            if(this.distance(0, 0, r, c) < cornerSize || this.distance(this.rows() - 1, this.cols() - 1, r, c) < cornerSize) {
+                this.terrain[r][c] = Tile.EMPTY;
+            }
+        }
+    }
 };
 
 Map.prototype.rows = function() {
-	return this.terrain.length;
+    return this.terrain.length;
 };
 
 Map.prototype.cols = function() {
-	return this.terrain[0].length;
+    return this.terrain[0].length;
 };
 
 /** Dumps the map state to a simpler JavaScript object
   * This is used to send the map over the network.
   */
 Map.prototype.dump = function() {
-	obj = {terrain: this.terrain};
+    obj = {terrain: this.terrain};
 
-	return obj;
+    return obj;
 };
 
 /** Loads the map from a JavaScript object */
 Map.prototype.load = function(obj) {
-	this.terrain = obj.terrain;
+    this.terrain = obj.terrain;
 };
 
 /** Gets the row and column of the hexagon that contains (px, py)
   * @returns {x: column, y: row}
   */
 Map.prototype.hexAt = function(px, py) {
-	// Calculate the size of the hexagons
-	var size = HEX_HEIGHT / 2;
-	
-	// Use that to find the approximate x and y of the tile
-	var y = 2/3 * (py - HEX_HEIGHT / 2) / size;
-	var x = 1/3 * (Math.sqrt(3) * px - py + HEX_HEIGHT / 2) / size - 0.5;
-	var z = -x - y; // calculate z, used later
-	
-	// Round each coordinate
-	var rx = Math.round(x);
-	var ry = Math.round(y);
-	var rz = Math.round(z);
-	
-	// Calculate deltas
-	var dx = Math.abs(rx - x);
-	var dy = Math.abs(ry - y);
-	var dz = Math.abs(rz - z);
-	
-	// Reverse the largest delta
-	if(dx > dy && dx > dz) {
-		rx = -ry - rz;
-	} else if(dy > dz) {
-		ry = -rx - rz;
-	}
-	
-	return {x: rx, y: ry};
+    // Calculate the size of the hexagons
+    var size = HEX_HEIGHT / 2;
+    
+    // Use that to find the approximate x and y of the tile
+    var y = 2/3 * (py - HEX_HEIGHT / 2) / size;
+    var x = 1/3 * (Math.sqrt(3) * px - py + HEX_HEIGHT / 2) / size - 0.5;
+    var z = -x - y; // calculate z, used later
+    
+    // Round each coordinate
+    var rx = Math.round(x);
+    var ry = Math.round(y);
+    var rz = Math.round(z);
+    
+    // Calculate deltas
+    var dx = Math.abs(rx - x);
+    var dy = Math.abs(ry - y);
+    var dz = Math.abs(rz - z);
+    
+    // Reverse the largest delta
+    if(dx > dy && dx > dz) {
+        rx = -ry - rz;
+    } else if(dy > dz) {
+        ry = -rx - rz;
+    }
+    
+    return {x: rx, y: ry};
 };
 
 /** Gets the row and column of the hexagon that contains (px, py) after reversing the transformations applied during rendering
   * See Map.render for what these transformations actually are
   */
 Map.prototype.hexAtTransformed = function(px, py, x, y, offsetx, offsety, scale) {
-	// Set defaults
-	offsetx = typeof offsetx !== "undefined" ? offsetx : 0;
-	offsety = typeof offsety !== "undefined" ? offsety : 0;
-	scale = typeof scale !== "undefined" ? scale : 1.0;
-	
-	return this.hexAt((px + offsetx - x) / scale, (py + offsety - y) / scale);
+    // Set defaults
+    offsetx = typeof offsetx !== "undefined" ? offsetx : 0;
+    offsety = typeof offsety !== "undefined" ? offsety : 0;
+    scale = typeof scale !== "undefined" ? scale : 1.0;
+    
+    return this.hexAt((px + offsetx - x) / scale, (py + offsety - y) / scale);
 };
 
 /** Renders the map on the given canvas context.
@@ -127,45 +127,45 @@ Map.prototype.hexAtTransformed = function(px, py, x, y, offsetx, offsety, scale)
   * @param scale The scale at which things are drawn. 1.0 is default if scale is not passed
   */
 Map.prototype.render = function(ctx, x, y, width, height, offsetx, offsety, scale) {
-	// Set defaults
-	offsetx = typeof offsetx !== "undefined" ? offsetx : 0;
-	offsety = typeof offsety !== "undefined" ? offsety : 0;
-	scale = typeof scale !== "undefined" ? scale : 1.0;
-	
-	// Calculate the width and height of each hexagon
-	var h = HEX_HEIGHT * scale;
-	var w = Math.round(Math.sqrt(3) / 2 * h);
-	
-	// Go through each hexagon on the map and see if it's in the window
-	for(var r = 0; r < this.rows(); r++) {
-		for(var c = 0; c < this.cols(); c++) {
-			// Determine the location of the top left bounding box of this hex
-			var hexx = (c + r / 2) * w - offsetx;
-			var hexy = (r * 3 / 4) * h - offsety;
-			
-			// Check if the hexagon is in the window
-			if(hexx + offsetx >= 0 && hexx <= width && hexy + offsety >= 0 && hexy <= height) {
-				// Set color
-				var color = Tile.properties[this.terrain[r][c]].color;
-				ctx.fillStyle = color;
-				ctx.strokeStyle = this.terrain[r][c] != Tile.EMPTY ? "#222222" : "#000000"; //hex border
-				
-				ctx.beginPath();
-				ctx.moveTo(x + hexx, y + hexy + h / 4);
-				ctx.lineTo(x + hexx + w / 2, y + hexy);
-				ctx.lineTo(x + hexx + w, y + hexy + h / 4);
-				ctx.lineTo(x + hexx + w, y + hexy + h * 3 / 4);
-				ctx.lineTo(x + hexx + w / 2, y + hexy + h);
-				ctx.lineTo(x + hexx, y + hexy + h * 3 / 4);
-				ctx.lineTo(x + hexx, y + hexy + h / 4);
-				
-				ctx.fill();
-				ctx.stroke();
-			}
-		}
-	}
+    // Set defaults
+    offsetx = typeof offsetx !== "undefined" ? offsetx : 0;
+    offsety = typeof offsety !== "undefined" ? offsety : 0;
+    scale = typeof scale !== "undefined" ? scale : 1.0;
+    
+    // Calculate the width and height of each hexagon
+    var h = HEX_HEIGHT * scale;
+    var w = Math.round(Math.sqrt(3) / 2 * h);
+    
+    // Go through each hexagon on the map and see if it's in the window
+    for(var r = 0; r < this.rows(); r++) {
+        for(var c = 0; c < this.cols(); c++) {
+            // Determine the location of the top left bounding box of this hex
+            var hexx = (c + r / 2) * w - offsetx;
+            var hexy = (r * 3 / 4) * h - offsety;
+            
+            // Check if the hexagon is in the window
+            if(hexx + offsetx >= 0 && hexx <= width && hexy + offsety >= 0 && hexy <= height) {
+                // Set color
+                var color = Tile.properties[this.terrain[r][c]].color;
+                ctx.fillStyle = color;
+                ctx.strokeStyle = this.terrain[r][c] != Tile.EMPTY ? "#222222" : "#000000"; //hex border
+                
+                ctx.beginPath();
+                ctx.moveTo(x + hexx, y + hexy + h / 4);
+                ctx.lineTo(x + hexx + w / 2, y + hexy);
+                ctx.lineTo(x + hexx + w, y + hexy + h / 4);
+                ctx.lineTo(x + hexx + w, y + hexy + h * 3 / 4);
+                ctx.lineTo(x + hexx + w / 2, y + hexy + h);
+                ctx.lineTo(x + hexx, y + hexy + h * 3 / 4);
+                ctx.lineTo(x + hexx, y + hexy + h / 4);
+                
+                ctx.fill();
+                ctx.stroke();
+            }
+        }
+    }
 };
 
 if(typeof exports === "object") {
-	module.exports = Map;
+    module.exports = Map;
 }

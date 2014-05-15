e8d403e38e82c07e3ca95d788edebe1bc581f844
gupti <paulgupti@gmail.com>
2014-05-14 20:42:24 -0400

    Added water tile.

--- static/tile.js
+++ static/tile.js
@@ -6,12 +6,14 @@ var Tile = {
 	EMPTY: 0, // outside map boundaries, not drawn; used to make maps look like a specific shape
 	NORMAL: 1, // basic tile
 	WALL: 2, // stone wall tile
+	WATER: 3,
 	
 	// Contains properties for all tiles
 	properties: {
 		0: {walkable: false, color: "#000000"},
 		1: {walkable: true, color: "#4CBB17"},
 		2: {walkable: false, color: "#444444"}
+		3: {walkable: true. color: "#1A6CD2"}
 	}
 };
 
